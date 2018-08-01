c98a47baed644788d47a1fbf23c618865c30f880
David Li <dbli2000@gmail.com>
2018-07-31 23:42:32 -0400

    Update Deconvolution.py
    Added modified Richardson Lucy function.

--- Python/Deconvolution.py
+++ Python/Deconvolution.py
@@ -19,30 +19,74 @@ Created on Tue Jul 31 09:11:07 2018
 """
 import skimage
 import numpy as np
+import numpy.random as npr
 import matplotlib.pyplot as plt
-import os
-from skimage import color, data, restoration, io
+from scipy.signal import fftconvolve, convolve
 from scipy.signal import convolve2d as conv2
 import csv
+
+def RichardsonLucy(image, psf, iterations=50, clip=False):
+    """
+    Richardson-Lucy deconvolution, modified from skimage to deal with 0s
+   
+    Inputs:
+        image : ndarray that is starting image
+        psf : ndarraym, the point spread function.
+        iterations : number of iterations
+        clip : boolean, optional. False by default. If true, pixel value of the result above 1 or
+                under -1 are thresholded for skimage pipeline compatibility.
+    Outputs: 
+        im_deconv : deconvolved image
+    """
+    # compute the times for direct convolution and the fft method. The fft is of
+    # complexity O(N log(N)) for each dimension and the direct method does
+    # straight arithmetic (and is O(n*k) to add n elements k times)
+    direct_time = np.prod(image.shape + psf.shape)
+    fft_time =  np.sum([n*np.log(n) for n in image.shape + psf.shape])
+
+    # see whether the fourier transform convolution method or the direct
+    # convolution method is faster (discussed in scikit-image PR #1792)
+    time_ratio = 40.032 * fft_time / direct_time
+
+    if time_ratio <= 1 or len(image.shape) > 2:
+        convolve_method = fftconvolve
+    else:
+        convolve_method = convolve
+
+    image = image.astype(np.float)
+    psf = psf.astype(np.float)
+    im_deconv = 0.5 * np.ones(image.shape)
+    psf_mirror = psf[::-1, ::-1]
+
+    for _ in range(iterations):
+        relative_blur = image / convolve_method(im_deconv, psf, 'same')
+        relative_blur[np.isnan(relative_blur)] = 0
+        im_deconv *= convolve_method(relative_blur, psf_mirror, 'same')
+
+    if clip:
+        im_deconv[im_deconv > 1] = 1
+        im_deconv[im_deconv < -1] = -1
+
+    return im_deconv
 
 def deconvolute(IntensityList, IterationNumber = 30, PercentageMin = 1/5.5):
     #Define point spread function
     psf = np.array([[1/37, 1/37, 1/37,1/37 ,1/37], [1/37, 2/37, 2/37, 2/37 ,1/37], [1/37, 2/37, 5/37, 2/37 ,1/37], [1/37, 2/37, 2/37, 2/37 ,1/37], [1/37, 1/37, 1/37, 1/37 ,1/37]])  
    
     # Restore image using Richardson-Lucy algorithm
-    DefaultDeconvoluted = restoration.richardson_lucy(IntensityList, psf, iterations=10, clip = False)
-    DeconvolutedIntensityList = restoration.richardson_lucy(IntensityList, psf, iterations=IterationNumber, clip  = False)
-    print(DefaultDeconvoluted)
+    DefaultDeconvoluted = RichardsonLucy(IntensityList, psf, iterations=10, clip = False)
+    DeconvolutedIntensityList = RichardsonLucy(IntensityList, psf, iterations=IterationNumber, clip  = False)
+
     #Plot deconvoluted images
     plt.set_cmap('jet')
     #Plot default deconvoluted image
-    #plt.imshow(DefaultDeconvoluted,vmin=DefaultDeconvoluted.max()*PercentageMin, vmax=DefaultDeconvoluted.max())
-    plt.imshow(DefaultDeconvoluted)
+    plt.figure(4)
+    plt.imshow(IntensityList)
+    plt.figure(2)
+    plt.imshow(DefaultDeconvoluted,vmin=DefaultDeconvoluted.max()*PercentageMin, vmax=DefaultDeconvoluted.max())
     plt.show()
-    plt.set_cmap('jet')
     #Plot user deconvoluted image
+    plt.figure(3)
     plt.imshow(DeconvolutedIntensityList,vmin=DeconvolutedIntensityList.max()*PercentageMin, vmax=DeconvolutedIntensityList.max())
-    plt.show() 
-    
     #Returns intensity list with user inputted number of iterations
     return DeconvolutedIntensityList
