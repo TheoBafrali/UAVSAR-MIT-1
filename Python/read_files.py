5473f36f0033415ac531afb8588fb0af37a20cdc
David Li <dbli2000@gmail.com>
2018-08-01 09:44:25 -0400

    Update read_files.py

--- Python/read_files.py
+++ Python/read_files.py
@@ -1,20 +1,35 @@
+'''
+This file is meant to read the data from the motion capture system, as well as from unpacked, pickle data from the PulsON 440 unpack script.
+
+@author: Mason
+'''
+
+#Import required modules
 import pandas as pd
 import pickle
 import numpy
 
-'''
-This file is meant to read the data from the motion capture system, as well as from unpacked, pickle data from the PulsON 440 unpack script.
-'''
 
 def read_motion_data(filename,rigid_body_name='Mason UAV-SAR 1'):
 
     '''
-Will return a list that holds x, y and z data from the motion capture system
+    Inputs:
+        filename = literally file name  of motion data, should be a .csv
+        rigid_body_name = rigid body you want to extract data for
+    
+    Outputs:
+        final_data: array containing x, y, and z positions for all time points of given marker
+    
+    Summary:
+        Will return a list that holds x, y and z data from the motion capture system
     '''
-
+    
+    #Reads in .csv
     data = []
     path = '../Raw_Data/' + str(filename) #This is the path to the file
     parsedData = pd.read_csv(path,low_memory=False,skiprows=3) #Reading the CSV file
+    
+    #Only adding data for the desired marker
     for name in list(parsedData):
         #Determining if the parsed data is part of the rigid body
 
@@ -39,14 +54,21 @@ Will return a list that holds x, y and z data from the motion capture system
 def read_radar_data(filename='../Raw_Data/data.pkl'):
 
     '''
-Will return a list with scan data, time stamp, and range bins from the pickled data
+    Inputs: 
+        filename: name of .pkl containing RADAR data
+    Outputs:
+        data: restructured set of arrays containing scan data, time stamps, and range bins
+    Summary:
+        Will return a list with scan data, time stamp, 
+        and range bins from the pickled data
     '''
-
+    #Load data
     path = '../Raw_Data/' + str(filename) #This is the path to the file
     data = pickle.load(open(path,'rb')) #Loading the pickle file
     time_stamp = numpy.concatenate(data['time_stamp'])
     scan_data = data['scan_data']
     range_bins = data['range_bins']
+    
     #Adding only the scan, time stamp, and range bin data to the final list
     new_time_stamp = []
 
