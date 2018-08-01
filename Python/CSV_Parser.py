4c31457e892de4b965dd40a8510dd05f6dcae9e6
David Li <dbli2000@gmail.com>
2018-08-01 09:23:46 -0400

    Update and rename david_time_align.py to CSV_Parser.py
    Added documentation

--- Python/CSV_Parser.py
+++ Python/CSV_Parser.py
@@ -1,21 +1,31 @@
+'''
+Reads in CSVs from David's manual alignment
+
+@author: David + Mason
+'''
+#Import required modules
 import pandas as pd
 import csv
+
+#Reads in Position data, generates list
 path = '../Raw_Data/DavidPosition.csv' 
 parsedData = pd.read_csv(path,low_memory=False, delimiter=',', header=0, encoding='ascii')
 X = parsedData['X']
 Y = parsedData['Y']
 Z = parsedData['Z']
 
+#Reads in pulse data from CSV
 path = '../Raw_Data/DavidPulseData.csv'
 Pulses = [] 
 with open(path, 'r') as csvfile:
     reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     for row in reader:
         Pulses.append(row)
+        
+#Reads in range axis data from csv
 path = '../Raw_Data/DavidRangeAxis.csv'
 rangeAxis = [] 
 with open(path,'r') as csvfile:
     reader = csv.reader(csvfile,delimiter= ' ', quotechar = '|')
     for row in reader:
         rangeAxis.append(int(row[0]))
-print(rangeAxis)
