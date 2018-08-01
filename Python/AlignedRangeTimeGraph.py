78bc381cebd7ec07c49e0a4022affe0410341f72
David Li <dbli2000@gmail.com>
2018-08-01 09:07:15 -0400

    Update AlignedRangeTimeGraph.py
    Added some documentation

--- Python/AlignedRangeTimeGraph.py
+++ Python/AlignedRangeTimeGraph.py
@@ -10,12 +10,21 @@ import numpy as np
 import math
 
 def AlignedGraph(aligned_data,radar_data):
+    '''
+    Inputs:
+        aligned_data: Contains arrays of pulses and positions for each pulse
+        radar_data: Contains a lot of stuff, but only the range bins are useful
     
+    Outputs/Summary:
+        Plots RTI graph with aligned data
+    '''
     Pulses = aligned_data[0]
     Range_Bins = radar_data[2]
     RangeBinDistance = Range_Bins[2]-Range_Bins[1]
     RangeBinX = []
     position = aligned_data[1]
+    
+    #Positions for each point
     box_position = [[.33,.168,-.149],[2.44,.168,-2.168],[.233,.168,-2.69],[2.48,.168,.859]]
     for i in np.arange(0,len(Range_Bins),50):
         RangeBinX.append(round(i*RangeBinDistance+Range_Bins[0],2))
@@ -31,6 +40,7 @@ def AlignedGraph(aligned_data,radar_data):
 
     print(distance)
     plt.set_cmap('nipy_spectral')
+    plt.figure(4)
     #plt.xticks(np.arange(0,len(Range_Bins),50),RangeBinX)
 #plt.yticks(np.arange(0,len(Pulses),1000),np.flip(np.arange(0,len(Pulses),1000),0))
     plt.xlabel("Distance (m)")
