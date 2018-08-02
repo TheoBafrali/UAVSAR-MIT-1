8ca4eb2cc0154e2410d726c12bb852442f0ee4fc
David Li <dbli2000@gmail.com>
2018-08-02 09:25:53 -0400

    Update main3D.py

--- Python/main3D.py
+++ Python/main3D.py
@@ -24,6 +24,10 @@ import matplotlib.pyplot as plt
 motion_data = read_motion_data("../Raw_Data/UAVSAR4Flight1.csv","UASSAR4")
 radar_data = read_radar_data("../Raw_Data/UAVSAR4Flight1.pkl")
 
+#Does RCS correction
+new_radar_data = rcs(radar_data)
+
+#Plots RTI graph
 plt.figure(0)
 plt.set_cmap('jet')
 rti_ax = plt.imshow(20 * np.log10(np.abs(new_radar_data[0])))
@@ -34,10 +38,10 @@ plt.ylabel('Pulse Index')
 cbar = plt.colorbar()
 cbar.ax.set_ylabel('dB')
 plt.show()
-#Calculates the right frames to perform alignment on
-#Currently using manual alignment,  instead of these functions
 
 '''
+#Calculates the right frames to perform alignment on
+#Currently using manual alignment,  instead of these functions
 motion_start = find_i_of_first_motion(motion_data)
 print(motion_start)
 motion_end = find_i_of_last_motion(motion_data)
@@ -49,18 +53,20 @@ print(radar_start)
 radar_start = 1
 motion_start = 1
 motion_end = 1
+
 #Aligns data, currently using frames given in function definition
-aligned_data = align_data(radar_data,motion_data,radar_start,motion_start, motion_end)
+# motion_point_one = 5100, motion_point_last = 29545,  radar_point_one = 1110
+aligned_data = align_data(new_radar_data,motion_data,radar_start,motion_start, motion_end)
 
 #Plots aligned graph
-AlignedGraph(aligned_data,radar_data)
-'''
+AlignedGraph(aligned_data,new_radar_data)
+
 #Calculates and plots BackProjected Image
 IntensityList = BackProjection(aligned_data,radar_data,[-5,-5],[5,5],0.25)
 
 #Deconvolutes image and plots images
 deconvolute(IntensityList, IterationNumber = 3, PercentageMin = 1/5.5)
-'''
+
 
 #Currently unused code
 '''
