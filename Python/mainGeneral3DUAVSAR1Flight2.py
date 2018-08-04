b6c81e5fad28689038c7c21851f1b97a336f7143
David Li <dbli2000@gmail.com>
2018-08-04 03:11:56 -0400

    Update mainGeneral3DUAVSAR1Flight2.py

--- Python/mainGeneral3DUAVSAR1Flight2.py
+++ Python/mainGeneral3DUAVSAR1Flight2.py
@@ -15,6 +15,7 @@ from Deconvolution import deconvolute
 from plotRTI import plotRTI
 from FastLinInt import FastBackProjection
 from helper_functions import linear_interp_nan
+import matplotlib.pyplot as plt
 
 #Converts motion and RADAR data to the right data structures
 unpack("../Raw_Data/uavsar1flight4")
@@ -45,7 +46,13 @@ AlignedGraph(aligned_data,radar_data, [[2.920519426,	0.089892255,	-1.116615139]]
 
 
 #Calculates and plots BackProjected Image
-IntensityList = FastBackProjection(aligned_data,radar_data,[-3,1.5],[0,4],0.005)
+IntensityList = FastBackProjection(aligned_data,radar_data,[-3,1.5],[0,4],0.01)
 
 #Deconvolutes image and plots images
-Image = deconvolute(IntensityList, IterationNumber = 1, PercentageMin = 1/15)
+Image = deconvolute(IntensityList, IterationNumber = 2, PercentageMin = 1/5)
+
+'''
+plt.figure(5)
+plt.imshow(Image)
+plt.show()
+'''
