e124a5ddb7f06050b92e704cf3e3fbde3b8c962b
David Li <dbli2000@gmail.com>
2018-08-04 02:41:53 -0400

    Update mainGeneral3D.py

--- Python/mainGeneral3D.py
+++ Python/mainGeneral3D.py
@@ -10,15 +10,18 @@ from read_files import read_radar_data, read_motion_data
 from rcs import rcs
 from data_align import align_data
 from GeneralAlignedRangeTimeGraph import AlignedGraph 
+from FastLinInt import FastBackProjection
 from LinInt import BackProjection
 from Deconvolution import deconvolute
 from plotRTI import plotRTI
+from helper_functions import linear_interp_nan
 
 #Converts motion and RADAR data to the right data structures
 unpack("../Raw_Data/uavsar1flight1")
 
 #Loads data from the RADAR
 motion_data = read_motion_data("../Raw_Data/UAVSAR1Flight1.csv","UAVSAR1")
+motion_data = linear_interp_nan(motion_data[1],motion_data[0])
 radar_data = read_radar_data("data.pkl", 580, 0.30) #580, 0.30 is probably good
 
 #Performs RCS correction
@@ -41,7 +44,8 @@ aligned_data = align_data(radar_data,motion_data,radar_start,motion_start, motio
 AlignedGraph(aligned_data,radar_data, [[.942713,.1,1.019]])
 
 #Calculates and plots BackProjected Image
-IntensityList = BackProjection(aligned_data,radar_data,[-4,0],[4,4],0.02)
-'''
+#IntensityList = BackProjection(aligned_data,radar_data,[-4,0],[4,4],0.1)
+IntensityList2 = FastBackProjection(aligned_data, radar_data, [-4,0],[4,4], 0.005)
+
 #Deconvolutes image and plots images
-Image = deconvolute(IntensityList, IterationNumber = 2, PercentageMin = 1/15)
+Image = deconvolute(IntensityList2, IterationNumber = 2, PercentageMin = 1/15)
